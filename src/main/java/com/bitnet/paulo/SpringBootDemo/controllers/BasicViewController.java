c74ae0cb1d9a9fd3cb7f62d9fc4b91c57f82fc6c
GiyuDev <rejecteddev@outlook.com>
2022-03-03 19:54:50 -0600

    Clase modelo para agregar posts desde una lista

--- src/main/java/com/bitnet/paulo/SpringBootDemo/controllers/BasicViewController.java
+++ src/main/java/com/bitnet/paulo/SpringBootDemo/controllers/BasicViewController.java
@@ -1,15 +1,30 @@
 package com.bitnet.paulo.SpringBootDemo.controllers;
 
+import com.bitnet.paulo.SpringBootDemo.model.BlogPost;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 @Controller
-@RequestMapping(path = "holahtml")
+@RequestMapping(path = "home")
 public class BasicViewController {
 
-    @GetMapping(path = {"saludar", "holamundo"})
-    public String hello() {
+    public List<BlogPost> getAllBlogPost() {
+        return List.of(
+                new BlogPost(1, "Post prueba", "Este es un post de prueba insertado desde un modelo", "http://localhost:8081/img/a.jpg", LocalDate.of(2022, Month.MARCH, 3)),
+                new BlogPost(2, "Post prueba 2", "Este es un post de prueba insertado desde un modelo 2", "http://localhost:8081/img/a.jpg", LocalDate.of(2022, Month.MARCH, 3)),
+                new BlogPost(2, "Post prueba 3", "Este es un post de prueba insertado desde un modelo 3", "http://localhost:8081/img/a.jpg", LocalDate.of(2022, Month.MARCH, 3))
+        );
+    }
+
+    @GetMapping(path = {"post", ""})
+    public String hello(Model model) {
+        model.addAttribute("posts", getAllBlogPost());
         return "index";
     }
 }
---
+++ src/main/java/com/bitnet/paulo/SpringBootDemo/model/BlogPost.java
@@ -0,0 +1,62 @@
+package com.bitnet.paulo.SpringBootDemo.model;
+
+import java.time.LocalDate;
+
+public class BlogPost {
+
+    private int id;
+    private String title;
+    private String text;
+    private String img_url;
+    private LocalDate date;
+
+    public BlogPost(int id, String title, String text, String img_url, LocalDate date) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.img_url = img_url;
+        this.date = date;
+    }
+
+    public BlogPost() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
